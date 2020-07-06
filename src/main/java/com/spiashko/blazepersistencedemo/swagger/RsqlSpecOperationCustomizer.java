e7f760645ea55eeaf4eb2cae19466092c750b1fb
Siarhei Piashko <Siarhei.Piashko@seavus.com>
2020-07-07 01:09:33 +0300

    refactoring

--- src/main/java/com/spiashko/blazepersistencedemo/swagger/RsqlSpecOperationCustomizer.java
+++ src/main/java/com/spiashko/blazepersistencedemo/swagger/RsqlSpecOperationCustomizer.java
@@ -1,11 +1,15 @@
 package com.spiashko.blazepersistencedemo.swagger;
 
+import com.spiashko.blazepersistencedemo.model.Cat;
 import com.spiashko.blazepersistencedemo.rsql.RsqlSpec;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
-import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.parameters.Parameter;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.SpringDocAnnotationsUtils;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.core.MethodParameter;
 import org.springframework.data.jpa.domain.Specification;
@@ -17,8 +21,12 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @Component
 public class RsqlSpecOperationCustomizer implements OperationCustomizer {
+
+    private final RsqlSpecOpenApiCustomiserRegistry registry;
+
     @Override
     public Operation customize(Operation operation, HandlerMethod handlerMethod) {
 
@@ -38,26 +46,33 @@ public class RsqlSpecOperationCustomizer implements OperationCustomizer {
 
     private void enrichOperationWithSpecParam(Operation operation, MethodParameter parameter) {
 
-        /**
-         * @Parameter(in = ParameterIn.QUERY
-         *         , name = "filter"
-         *         , description = "Filter criteria in RSQL/FIQL format."
-         *         , content = @Content(schema = @Schema(type = "string"))
-         *         , example = "firstName==john;lastName==doe"
-         *         , style = ParameterStyle.FORM
-         *         , explode = Explode.TRUE
-         * )
-         */
-
         RsqlSpec rsqlSpecAnnotation = Objects.requireNonNull(parameter.getParameterAnnotation(RsqlSpec.class));
 
-        Parameter parameterHeader = new Parameter()
+        Parameter parameterFilter = new Parameter()
                 .in(ParameterIn.QUERY.toString())
+                .required(false)
                 .name(rsqlSpecAnnotation.requestParamName())
-                .description("Filter criteria in RSQL/FIQL format.")
-                .schema(new StringSchema().addEnumItem("v1")._default("v1").name("Accept-version"))
-                .description("Custom Header To be Pass");
+                .schema(new StringSchema());
 
-        operation.addParametersItem(parameterHeader);
+        Class<?> clazz = getEntityClass(operation, parameter);
+
+        registry.addCustomiser(
+                (OpenAPI openApi) -> {
+                    Schema<?> schema = SpringDocAnnotationsUtils.extractSchema(openApi.getComponents(), clazz, null);
+                    String schemaRef = schema.get$ref();
+                    String schemaName = schemaRef.substring(schemaRef.lastIndexOf('/') + 1);
+                    parameterFilter.addExtension("x-filtered-object-schema", schemaRef);
+                    parameterFilter.setDescription("Filter criteria in RSQL/FIQL format. " +
+                            "Property names based on `" + schemaName + "` schema. " +
+                            "Symbol '.'(dot) should be used for nested properties.");
+                }
+        );
+
+        operation.addParametersItem(parameterFilter);
     }
+
+    private Class<?> getEntityClass(Operation operation, MethodParameter parameter) {
+        return Cat.class;
+    }
+
 }
